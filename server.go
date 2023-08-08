afd3b68c33cb3e656f0000babadaaa78b1336765
Quirxx2 <siarheiii@gmail.com>
2023-08-08 10:17:05 +0300

    First task

---
+++ server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}).Methods("GET")
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8000",
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
